05a1b2454217f5889e5b38a19a88b72d0b4673b5
Leonardo Carvalho <0leonardocarvalho@gmail.com>
2024-05-22 10:11:24 -0300

    criando as seções e o contador com js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('Olá, mundo');
+const dataDoEvento = new Date("Apr 12, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `Faltam ${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s para o meu ANIVERSÁRIO!`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+
+        document.getElementById('contador').innerHTML = 'Evento expirado, em breve será adicionado outra data';
+    }
+    
+}, 1000);
